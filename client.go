ab2209b4cdc3f3d7dac4931feb5134636032f305
ivanzz <ivan@alphaorbeta.com>
2024-09-28 10:10:05 +0800

    Add registerSignerFn

--- client.go
+++ client.go
@@ -2,12 +2,18 @@ package ethclient
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"fmt"
 	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum/log"
+
 	"github.com/ChewZ-life/ethclient/nonce"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 )
@@ -16,6 +22,7 @@ type Client struct {
 	*ethclient.Client
 	rpcClient *rpc.Client
 	nonce.Manager
+	signers []bind.SignerFn // Method to use for signing the transaction (mandatory)
 }
 
 func Dial(rawurl string) (*Client, error) {
@@ -49,6 +56,60 @@ func (c *Client) RawClient() *ethclient.Client {
 
 func (c *Client) SetNonceManager(nm nonce.Manager) {
 	c.Manager = nm
+}
+
+func (c *Client) GetSigner() bind.SignerFn {
+	// combine all signerFns
+	return func(a common.Address, t *types.Transaction) (tx *types.Transaction, err error) {
+		if len(c.signers) == 0 {
+			return nil, fmt.Errorf("no signerFn registered")
+		}
+
+		for i, fn := range c.signers {
+			tx, err = fn(a, t)
+			log.Debug("try to call signerFn", "index", i, "err", err, "account", a)
+
+			if err != nil {
+				continue
+			}
+
+			return tx, nil
+		}
+
+		return nil, bind.ErrNotAuthorized
+	}
+}
+
+func (c *Client) RegisterSigner(signerFn bind.SignerFn) {
+	log.Info("register signerFn for signing...")
+	c.signers = append(c.signers, signerFn)
+}
+
+// Registers the private key used for signing txs.
+func (c *Client) RegisterPrivateKey(ctx context.Context, key *ecdsa.PrivateKey) error {
+	chainID, err := c.ChainID(ctx)
+	if err != nil {
+		return err
+	}
+	keyAddr := crypto.PubkeyToAddress(key.PublicKey)
+	if chainID == nil {
+		return bind.ErrNoChainID
+	}
+	signer := types.LatestSignerForChainID(chainID)
+	signerFn := func(address common.Address, tx *types.Transaction) (*types.Transaction, error) {
+		if address != keyAddr {
+			return nil, bind.ErrNotAuthorized
+		}
+		signature, err := crypto.Sign(signer.Hash(tx).Bytes(), key)
+		if err != nil {
+			return nil, err
+		}
+		return tx.WithSignature(signer, signature)
+	}
+
+	c.RegisterSigner(signerFn)
+
+	return nil
 }
 
 func (c *Client) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
